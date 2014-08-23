a43d8f2fdbcd281963f15d7baa8332a1396571db
楠本健佑 <kensuke@kusumotokenyuu-no-MacBook-Air.local>
2014-08-24 00:30:16 +0900

    Initial Commit

---
+++ test01/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test01
+//
+//  Created by 楠本健佑 on 2014/08/24.
+//  Copyright (c) 2014年 楠本健佑. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
