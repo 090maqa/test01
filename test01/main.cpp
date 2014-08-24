f40eabf998a86b1f4f659b463c569a752c3aaccc
楠本健佑 <kensuke@kusumotokenyuu-no-MacBook-Air.local>
2014-08-24 22:50:40 +0900

    first test commit.

--- test01/main.cpp
+++ test01/main.cpp
@@ -6,13 +6,30 @@
 //  Copyright (c) 2014年 楠本健佑. All rights reserved.
 //
 
+#include <cassert>
+#include <fstream>
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "datatable.h"
+#include "mylibrary.h"
+
+
+/******************** main ********************/
+
+int main (void)
+{
+    PrintCurrentDirectory();
+    
+    DataTable combat_log;
+    
+    if (!combat_log.ReadCsv("data/海戦・ドロップ報告書.csv")) {
+        std::cout << "File cannot open." << std::endl;
+        return 1;
+    }
+    
+    //combat_log.PrintDataTable();
+    
     return 0;
 }
 
